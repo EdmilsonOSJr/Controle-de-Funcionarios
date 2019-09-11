289b8744a23171b28660001d5282d90eab05a9e5
EdmilsonOSJr <edmilsonbrodi@gmail.com>
2019-09-11 08:55:44 -0300

    Arrumei o menu, falta o buffer e a documentação. Foi criada a função verificaDigito()

--- funcoesVerificacao.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCOESVERIFICACAO_H_INCLUDED
-#define FUNCOESVERIFICACAO_H_INCLUDED
-
-int VerificaArquivoVazio(FILE*);
-int VerificaCpf(FILE*,char *);
-int verficaData(char *);
-void RetiraSequenciaDeEscape(char *);
-
-#endif // FUNCOESVERIFICACAO_H_INCLUDED
